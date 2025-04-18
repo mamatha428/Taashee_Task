7ed94f2ba8790e04a1621b07a67d569fafc83485
mamatha428 <133845223+mamatha428@users.noreply.github.com>
2025-04-18 18:42:33 +0530

    Add files via upload

--- student_course_database.sql
+++ student_course_database.sql
@@ -1,88 +1,116 @@
 create database Training;
-
 use Training;
-
 create table Hobby(
-   id int primary key auto_increment,
-   name varchar(32) not null,
-   added_by varchar(64) not null,
-   type char(1) default "I"
-   );
-   
-   insert into Hobby values(1,"journaling","mamatha","I");
-   
-   select *from Hobby;
-   
-   
-   alter table Hobby add column username varchar(32);
-   
-   select *from Hobby;
-   
-   update Hobby set username=lower(added_by);
-   
-   UPDATE Hobby
-SET added_by = CONCAT(UPPER(SUBSTRING(added_by, 1, 1)), LOWER(SUBSTRING(added_by, 2)));
-
-update Hobby set name="journaling" where id=1;
-
-update Hobby set name="badminton" where id=2;
-update Hobby set name="helping others" where id=3;
-
-alter table Hobby add column password varchar(32);
-
-update Hobby set password="password";
-
+id int primary key auto_increment,
+name varchar(32) not null,
+added_by varchar(64) not null,
+type varchar(1) default "I"
+);
+select *from Hobby;
 
 create database journaling_app;
 
 use journaling_app;
-
 create table users(
-   user_id int  auto_increment primary key,
-   username varchar(32) not null,
-   password varchar(32) not null
-   );
-   
-   CREATE TABLE entries (
-    entry_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    title VARCHAR(100),
-    content TEXT,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
-   
-   select *from users;
-   
-   select *from entries;
-   
-   insert into users values(3,"miheera","password");
-   
-   insert into users values(4,"arjun","password");
-   
-   desc student;
-   
-   insert into student values(1,"mamatha");
-   insert into student values(2,"kumar");
-   desc course;
-   insert into instructor values(1,"ranjith");
-   insert into course values(1,"machine learning",1);
-   
-   select *from student;
-   
-   select *From instructor;
-   
-   select *from course;
-   
-   select *from course_students;
-   
-   
-   
-   
+  user_id int auto_increment primary key,
+  username varchar(32) not null,
+  password varchar(32) not null
+  );
+  
+  insert into users values(1,"mamatha","password");
+  insert into users values(2,"kumar","kumar123");
+  
+  select *from users;
+  
+  create table entries(
+     entry_id int auto_increment primary key,
+     user_id int,
+     title varchar(100),
+     content text,
+     timestamp timestamp default current_timestamp,
+     foreign key(user_id) references users(user_id)
+     );
+     
+     
+     select *from entries;
+     
+     alter table Hobby add column username varchar(64);
+     
+     update Hobby set username=added_by;
+     
+     alter table Hobby add column password varchar(64);
+     
+     update Hobby set password="password";
+     
+     select *from Hobby;
+     
+     alter table Hobby add column role varchar(16);
+     
+     update Hobby set role="user";
+     
+     update Hobby set role="admin" where id=6;
+     
+     update Hobby set password="$2a$10$gSUKSRAtwMi1aKmHQj2YwOtdaLuLix8AFt2rCD/WCrQtelM3Gb6QG";
+     
+     update Hobby set role=upper(role);
+     
+     
+     alter table users add column role varchar(16);
+     
+     update users set role="USER";
+     
+     select *from users;
+     
+     update users set role="ADMIN" where user_id=1;
+     
+     update users set password="password" where user_id in(2,6);
+     
+     alter table users modify column password varchar(128);
+     
+     update users set password="$2a$09$DjHJ5cs.wUOsFuFqOmDc2.5NSlzz6um18zMPP6Vtv9ooFk4s5eDsu";
+     
+     delete from Hobby where id in(16,17);
+     
+     alter table Hobby modify column role varchar(16) default "USER";
+     
+     show tables;
+     
+     select *from role;
+     
+     delete from role where id=0;
+     
+     desc instructor;
+     
+     desc course;
+     
+     desc student;
+     
+     
+     select *from student;
+     
+     select *from course;
+     
+     select *from instructor;
+     
+     select *from course_students;
+     
+     select *from user;
+     
+     select *from user_roles; 
+     
+     select *from role;
+     
+     update user set password="$2a$10$MzosjT.6q/YS3EUJG39HpO0te03RRDkTP1mYFod.okb4DtC7uOkWq";
+     
+     update student set user_id=1 where id=1;
+     
+     update student set user_id=2 where id=2;
+     
+     
+      
+     
+  
+  
+  
 
 
-
-   
-   
-   
-   
